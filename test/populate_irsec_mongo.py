5c524ecdb30c2b966b743e9b7d7da8e0b00a8b04
koalatea <koalateac@gmail.com>
2018-04-21 03:06:13 -0400

    added for testing and fixing irsec subnets

--- test/populate_irsec_mongo.py
+++ test/populate_irsec_mongo.py
@@ -7,8 +7,9 @@ from werkzeug.security import generate_password_hash
 subnet_objs = []
 for i in range(1,11):
     subnet_objs.append(('Team{}'.format(i), '10.2.{}.0'.format(i) ,24))
-    subnet_objs.append(('Team{} vpc'.format(i), '172.16.{}.0'.format(i), 24))
+    subnet_objs.append(('Team{} vpc'.format(i), '172.21.{}.0'.format(i), 24))
     print(i)
+subnet_objs.append(('TEST', '172.16.156.0', 24))
 
 def insert_data(db):
     subnets = db.subnets
