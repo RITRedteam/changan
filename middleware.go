d44a874f24e5918be5d8908f4f31b2baf8051770
koalatea <koalateac@gmail.com>
2018-04-21 03:58:00 -0400

    added ip tracking in log

--- middleware.go
+++ middleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 	"time"
 
@@ -45,7 +46,8 @@ func (app *App) LogRequest(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		inner.ServeHTTP(w, r)
-		log.Printf("%s\t%s\t%s\t%s", r.Method, r.RequestURI, name, time.Since(start))
+		ip, _, _ := net.SplitHostPort(r.RemoteAddr)
+		log.Printf("%s\t%s\t%s\t%s\t%s", r.Method, r.RequestURI, name, time.Since(start), ip)
 	})
 }
 
